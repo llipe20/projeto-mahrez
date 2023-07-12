0a0c2ecc2090556dc95c15b2ec7198142a348fbf
Felipe <silvahlipe@gmail.com>
2023-07-12 14:00:31 -0300

    commit

--- html/home/function/extrato.php
+++ html/home/function/extrato.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../css/style-mobile/all.css" media="all">
-    <link rel="stylesheet" href="../../../css/style-mobile/function.css" media="screen and (max-width: 800px)">
+    <?php echo '<link rel="stylesheet" href="../../css/style-mobile/all.css" media="all">
+    <link rel="stylesheet" href="../../../css/style-mobile/function.css" media="screen and (max-width: 800px)">';?>
     <title>Extrato - Mahrez</title>
 </head>
 <body>
--- html/home/function/settings.php
+++ html/home/function/settings.php
@@ -1,49 +1,56 @@
+<?php
+session_start();
+echo '
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../css/style-mobile/login.css" media="screen and (max-width: 800px)">
+    <link rel="stylesheet" href="../../../css/style-mobile/function.css" media="screen and (max-width: 800px)">
     <title>Configurações</title>
 </head>
 <body>
-        <!-- CAIXA DE APRESENTAÇÃO  -->
-        <div class="box-terciario">
-            <img class="photo-litle" src="../imagem/form.png" alt="formulario">
+    <div class="box-principal">
 
-            <h1>Configurações</h1>
+        <!-- CAIXA DE APRESENTAÇÃO  -->
+        <div class="box-header-hora-extra">
+            <h1>Veja suas informações</h1>
         </div>
         <!-- CAIXA DE INFORMAÇÕES -->
-        <form class="box-novo-usuario" action="../../../php/home/function/configuracao.php" method="POST">
-            
-            <h2>Veja suas informações</h2>
+        <form class="box-conteudo" action="../../../php/home/function/configuracao.php" method="POST">
 
             <div class="box-text">
-            <label for="nome">User name:</label>
-            <input class="box-input-set" type="text" name="nome" value="<?php session_start(); echo $_SESSION['usuario'];?>">
+                <label for="nome">User name:</label>
+                <input class="input-texto" type="text" name="nome" value="'. $_SESSION['usuario'].'">
             </div>
 
             <div class="box-text">
-            <label for="email">E-mail:</label>
-            <input class="box-input-set" type="email" name="email" value="<?php echo $_SESSION['email'];?>">
+                <label for="email">E-mail:</label>
+                <input class="input-texto" type="email" name="email" value="'.$_SESSION['email'].'">
             </div>
 
             <div class="box-text">
-            <label for="valor-hora">Valor da hora:</label>
-            <input class="box-input-set" type="number" step="0.1" name="valor-hora" value="<?php echo $_SESSION['valorHora'];?>">
+                <label for="valor-hora">Valor da hora:</label>
+                <input class="input-time" type="number" step="0.1" name="valor-hora" value="'.$_SESSION['valorHora'].'">
+            </div>
 
             <div class="box-text">
-            <label for="senha">Password:</label>
-            <input class="box-input-set" type="password" name="senha" value="<?php echo $_SESSION['email'];?>">
+                <label for="senha">Password:</label>
+                <input class="input-texto" type="password" name="senha" value="'.$_SESSION['email'].'">
+            </div>
 
-            <div class="box-text">
-            <label for="confirm-password">Confirm password:</label>
-            <input class="box-input-set"  type="password" name="confirm-password" value="<?php echo $_SESSION['email'];?>">
-        
-            <input class="box-button" type="submit" name="button-salvar" value="Salvar">
+            <div class="box-tet">
+            <label for="confirm-password">Confirm pass:</label>
+            <input class="input-texto"  type="password" name="confirm-password" value="'.$_SESSION['email'].'">
+            </div>
+
+            <div class="box-button">
+            <input class="button" type="submit" name="button-salvar" value="Salvar">
 
             <a class="link-voltar" href="../home.php">Voltar</a>   
+            </div>
         </form>
     </div>
 </body>
-</html>
+</html>';
+?>
